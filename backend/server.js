8884c7853969364a9f790e648a145552c25859fb
Shuhail Ahmed <shuhailahmed202020@gmail.com>
2026-03-07 13:28:33 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 
 import commentRoutes from "./routes/comments.js";
 import tributeRoutes from "./routes/tributes.js";
+
+dotenv.config(); // .env load karega
 
 const app = express();
 
@@ -11,14 +14,13 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(
-    "mongodb+srv://admin:admin123@cluster0.r4mgez3.mongodb.net/memorialDB?retryWrites=true&w=majority",
-  )
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
+
 app.use("/api/comments", commentRoutes);
 app.use("/api/tributes", tributeRoutes);
 
-app.listen(5000, "0.0.0.0", () => {
+app.listen(process.env.PORT || 5000, "0.0.0.0", () => {
   console.log("Server running on port 5000");
 });
